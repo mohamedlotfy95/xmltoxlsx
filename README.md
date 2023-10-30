39008aba93443e11c3f4ff07ae23d84359a700b6
Lotfy <az_lotfy@outlook.com>
2023-10-30 23:03:53 +0200

    Code

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# xmltoxlsx
-python script that reads the following XML file and creates an array of objects where each object  represents one key/value pair, then print the array to the Console in a tabular format. 
+# XML Data Parser
+
+This Python script reads XML data from a file, parses it, and writes key-value pairs from the data to an Excel file.
+
+## Dependencies
+
+- **Python**: Make sure you have Python installed on your machine.
+- **xml.etree.ElementTree**: This is part of Python's standard library and provides methods to parse XML files.
+- **pandas**: This is a powerful data manipulation library that provides flexible data structures to manipulate and analyze data. You can install it using pip: `pip install pandas`.
+- **openpyxl**: This is a Python library to read/write Excel 2010 xlsx/xlsm/xltx/xltm files. You can install it using pip: `pip install openpyxl`.
+
+## How to Run
+
+1. Save the script in a .py file, for example, `xml_parser.py`.
+2. Place your XML file (e.g., `xmsource.xml`) in the same directory as your Python script.
+3. Open a terminal/command prompt.
+4. Navigate to the directory containing your script and XML file.
+5. Run the script using Python by typing `python xml_parser.py` and pressing Enter.
+
+## Example Output
+
+The script will create an Excel file named "tabula.xlsx" in the same directory as your Python script. The Excel file will have two columns: "Key" and "Value". Each row in the Excel file corresponds to a key-value pair from the XML data.
+
+If you run the script again with the same filename, it will ask you whether you want to overwrite the existing file or append new data.
+
+☠️
---
+++ xml_parser.py
@@ -0,0 +1,57 @@
+# Import the required libraries
+import xml.etree.ElementTree as ET
+import pandas as pd
+import os
+
+# Function to read XML data from a file
+def read_xml_file(file_path):
+    """Reads an XML file and returns its content."""
+    with open(file_path, 'r') as file:
+        return file.read()
+
+# Function to parse XML data
+def parse_xml_data(xml_data):
+    """Parses XML data and returns the root element."""
+    return ET.fromstring(xml_data)
+
+# Function to get key-value pairs from the XML data
+def get_key_value_pairs(root):
+    """Extracts key-value pairs from the XML data and returns them as a list."""
+    data = []
+    for elem in root.iter('add'):
+        data.append([elem.attrib['key'], elem.attrib['value']])
+    return data
+
+# Function to write data to an Excel file
+def write_to_excel(data, file_name):
+    """Writes data to an Excel file. If the file already exists, asks the user whether to overwrite or append."""
+    df = pd.DataFrame(data, columns=['Key', 'Value'])
+    
+    if os.path.exists(file_name):
+        print(f"'{file_name}' already exists.")
+        choice = input("Enter 'O' to overwrite or 'A' to append: ").upper()
+        
+        if choice == 'O':
+            df.to_excel(file_name, index=False)
+            print(f"Data has been written to '{file_name}'.")
+        elif choice == 'A':
+            df_old = pd.read_excel(file_name)
+            df_new = pd.concat([df_old, df])
+            df_new.to_excel(file_name, index=False)
+            print(f"Data has been appended to '{file_name}'.")
+        else:
+            print("Invalid choice. No data has been written.")
+    else:
+        df.to_excel(file_name, index=False)
+        print(f"Data has been written to '{file_name}'.")
+
+# Main function to orchestrate the execution of the script
+def main():
+    """Main function that orchestrates the reading, parsing, and writing operations."""
+    xml_data = read_xml_file('xmsource.xml')
+    root = parse_xml_data(xml_data)
+    data = get_key_value_pairs(root)
+    write_to_excel(data, 'tabula.xlsx')
+
+if __name__ == "__main__":
+    main()
